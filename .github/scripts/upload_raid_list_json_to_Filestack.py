637f6d1cf9b5c10759d0790398486dd334003fab
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2023-08-28 16:15:49 +0200

    Update upload_raid_list_json_to_Filestack.py

--- .github/scripts/upload_raid_list_json_to_Filestack.py
+++ .github/scripts/upload_raid_list_json_to_Filestack.py
@@ -1,8 +1,12 @@
 from filestack import Security, Client, Filelink
+import os
 import time
 
 raid_list_file = "./community-gathered data/raid_list.json"
 policy = {"expiry": int(time.time())+4000}
+
+app_secret = os.environ.get("app_secret")
+handle = os.environ.get("handle")
 
 security = Security(policy, app_secret)
 filelink = Filelink(handle)
