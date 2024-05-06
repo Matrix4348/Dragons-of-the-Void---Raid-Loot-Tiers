f7f2420a45f724a545e99a201d50a61e279e784a
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2024-05-06 10:20:44 +0200

    Add "???" as tiers before an after fair share when no raid tiers have been provided

--- .github/scripts/create_raid_list.py
+++ .github/scripts/create_raid_list.py
@@ -109,19 +109,21 @@ for r in raid_list:
                 f.close()
             l=len(raid_list[r][d+" tiers"]) # This will be used several times below.
             if l==0:
-                raid_list[r][d+" tiers"].append(raid_list[r]["FS"][d])
-                l+=1
-                raid_list[r]["Common"][d].append("?")
-                raid_list[r]["Rare"][d].append("?")
-                raid_list[r]["Mythic"][d].append("?")
-                if raid_list[r]["Has hidden loot?"]== False:
-                    raid_list[r]["Hidden"][d].append("0")
-                else:
-                    raid_list[r]["Hidden"][d].append("?")
-                if raid_list[r]["Has summoner loot?"]== False:
-                    raid_list[r]["Summoner"][d].append("0")
-                else:
-                    raid_list[r]["Summoner"][d].append("?")
+                z=["???",raid_list[r]["FS"][d],"???"]
+                for j in range(3):
+                    raid_list[r][d+" tiers"].append(z[j])
+                    l+=1
+                    raid_list[r]["Common"][d].append("?")
+                    raid_list[r]["Rare"][d].append("?")
+                    raid_list[r]["Mythic"][d].append("?")
+                    if raid_list[r]["Has hidden loot?"]== False:
+                        raid_list[r]["Hidden"][d].append("0")
+                    else:
+                        raid_list[r]["Hidden"][d].append("?")
+                    if raid_list[r]["Has summoner loot?"]== False:
+                        raid_list[r]["Summoner"][d].append("0")
+                    else:
+                        raid_list[r]["Summoner"][d].append("?")
             # Tiers, as a single character string:
             raid_list[r][d]=""
             if raid_list[r][d+" tiers"][0]=="?" and raid_list[r]["FS"][d] not in raid_list[r][d+" tiers"]:
