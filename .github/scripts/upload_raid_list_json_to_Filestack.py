594d6302c3c58948824e59988c10e55023ee0fbd
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2024-05-16 16:44:16 +0200

    Delete file

--- .github/scripts/upload_raid_list_json_to_Filestack.py
+++ .github/scripts/upload_raid_list_json_to_Filestack.py
@@ -7,9 +7,10 @@ policy = {"expiry": int(time.time())+4000}
 
 app_secret = os.environ.get("app_secret")
 handle = os.environ.get("handle")
+api_key= os.environ.get("api_key")
 
 security = Security(policy, app_secret)
 filelink = Filelink(handle)
 
 #filelink.overwrite(filepath=raid_list_file,security=security)
-filelink.delete(apikey=app_secret,security=security)
+filelink.delete(apikey=api_key,security=security)
