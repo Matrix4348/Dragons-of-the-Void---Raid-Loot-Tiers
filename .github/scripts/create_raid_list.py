46cb7aafbdfdd80499c293e90b0c9dcb555edadc
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2024-06-04 21:51:47 +0200

    Update create_raid_list.py

--- .github/scripts/create_raid_list.py
+++ .github/scripts/create_raid_list.py
@@ -18,10 +18,10 @@ def itc(n): # Takes a number and writes it with comma separators.
     else:
         return "{:,}".format(int(n))
 
-base_file = "./community-gathered data/Basic raid data.csv"
-damage_file = "./community-gathered data/Base damage taken.csv"
-loot_path = "./community-gathered data/Loot tiers and drop data/"
-output_file = "./community-gathered data/raid_list.json"
+base_file = "./main/community-gathered data/Basic raid data.csv"
+damage_file = "./main/community-gathered data/Base damage taken.csv"
+loot_path = "./main/community-gathered data/Loot tiers and drop data/"
+output_file = "./main/community-gathered data/raid_list.json"
 
 default_dict = defaultdict(list)
 
