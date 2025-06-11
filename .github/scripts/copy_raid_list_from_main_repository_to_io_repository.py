e3501c74c5387387a2cbbfdf49e3c08b6a289d88
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2025-06-11 18:37:17 +0200

    Delete .github/scripts/copy_raid_list_from_main_repository_to_io_repository.py

--- .github/scripts/copy_raid_list_from_main_repository_to_io_repository.py
+++
@@ -1,7 +0,0 @@
-import shutil
-
-# Assuming that the main repository is located in ./main and the github.io repository is located in ./io:
-output_file = "./main/community-gathered data/raid-list.json" # Must exist
-io_output_file = "./io/dragons-of-the-void/raid-list.json"
-
-shutil.copy2(output_file, io_output_file)
